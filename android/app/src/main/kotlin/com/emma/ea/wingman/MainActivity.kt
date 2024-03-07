6e8573cb8d8307be0b7f23ba51ad856a3ee813d4
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2024-03-07 07:22:46 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/emma/ea/wingman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.emma.ea.wingman.wingman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:wingman/app/app.dart';
+import 'package:wingman/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
